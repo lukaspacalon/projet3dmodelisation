a66e5f1b4897906c3a123b80806ad82dd1c186bb
Lukas <lukaspacalon@hotmail.fr>
2021-12-18 17:26:28 +0100

    movement(real)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,5 +19,11 @@ public class PlayerController : MonoBehaviour
     {
         float xMov = Input.GetAxisRaw("Horizontal");
         float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+        motor.Move(velocity);
     }
 }
